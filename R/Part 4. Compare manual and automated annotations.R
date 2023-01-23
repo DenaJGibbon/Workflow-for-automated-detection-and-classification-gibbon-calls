c166ccb0d40507956a21042190873beafe844672
Dena Clink <denaclink@Denas-MacBook-Pro.local>
2023-01-23 09:23:44 -0500

    Update code.

--- R/Part 4. Compare manual and automated annotations.R
+++ R/Part 4. Compare manual and automated annotations.R
@@ -7,7 +7,11 @@ library(ggpubr)
 # NOTE: You must change this to the location where you have stored the downloaded data
 input.dir <- '/Volumes/DJC Files/Clink et al Zenodo Data/'
 
-# Get full file paths
+# Get full file paths for LTSA annotations
+gibbon.files <- list.files(paste(input.dir,'DataSheets/GibbonAnnotationsLTSA',sep=''),full.names = T)
+gibbon.files.short <- list.files(paste(input.dir,'DataSheets/GibbonAnnotationsLTSA',sep=''),full.names = F)
+
+# Get full file paths for detection tables
 ValidationTables <- 
   list.files(paste(input.dir,'DataSheets/TrueFalsePositiveTables',sep=''),
              full.names = T)
@@ -28,18 +32,20 @@ ValidationDFCorrect <-
   subset(ValidationDF,target.signal=='y'|target.signal=='o'|target.signal=='oo')
 
 # How many correct?
-nrow(ValidationDFCorrect) # 4372; 
+nrow(ValidationDFCorrect) # 3662
 
 # Calculate precision
 nrow(ValidationDFCorrect)/nrow(ValidationDF)
 
 
-start.time.mins <- ValidationDFCorrect$start.time/60
+# Convert detections to standardized format -------------------------------
+# Isolate start hour
 ValidationDFCorrect$start.time.mins <-  as.numeric(substr(ValidationDFCorrect$time,start=1,stop=1))
 
+# Loop to convert time to HH:MM:SS format; date is converted to today's date
 ValidationDFupdated <- data.frame()
-for(z in 1:nrow(ValidationDF)){
-  
+for(z in 1:nrow(ValidationDFCorrect)){
+  print(z)
   TempRow <- ValidationDFCorrect[z,]
   TempStart <- round(TempRow$start.time/60,2)
   
@@ -78,14 +84,17 @@ for(z in 1:nrow(ValidationDF)){
   ValidationDFupdated <- rbind.data.frame(ValidationDFupdated,TempRow)
 }
 
+# Check output
 head(ValidationDFupdated)
 
+# Separate start time
 ValidationDFupdated$TempStartTime <- str_split_fixed(ValidationDFupdated$TempStartTime,pattern = ' ',n=2)[,2]
+
+# Convert to hour only
 ValidationDFupdated$TempStartHour <- str_split_fixed(ValidationDFupdated$TempStartTime,pattern = ':',n=2)[,1]
 
 
-gibbon.files <- list.files(paste(input.dir,'DataSheets/GibbonAnnotationsLTSA',sep=''),full.names = T)
-gibbon.files.short <- list.files("/Users/denaclink/Desktop/LTSA_detections/gibbons",full.names = F)
+# Convert LTSA data to standardized format --------------------------------
 Recorder.gibbon <- str_split_fixed(gibbon.files.short,pattern = '_',n=2)[,1]
 gibbon.annotations <- data.frame()
 for(a in 1:length(gibbon.files)){
@@ -103,23 +112,22 @@ for(a in 1:length(gibbon.files)){
   gibbon.annotations <- rbind.data.frame(gibbon.annotations,temp.gibbon)
 }
 
+# Focus only on duet
 gibbon.annotations <- 
   droplevels(subset(gibbon.annotations,Call.type=='duet' ))
 
+# Check output
 head(gibbon.annotations)
 
+# Convert both LTSA and detection data to standardized time 
 gibbon.annotations$Start.time <- strptime(gibbon.annotations$Start.time,format = '%H:%M:%S' )
 gibbon.annotations <- na.omit(gibbon.annotations)
 gibbon.annotations$Start.time <-as.POSIXct(gibbon.annotations$Start.time)
-gghistogram(data=gibbon.annotations, x='Start.time')
 
 
 ValidationDFupdated$Start.time <- strptime(ValidationDFupdated$TempStartTime,format = '%H:%M:%S' )
 ValidationDFupdated <- na.omit(ValidationDFupdated)
 ValidationDFupdated$Start.time <-as.POSIXct(ValidationDFupdated$Start.time)
-
-gghistogram(data=ValidationDFupdated,x='Start.time')
-
 
 ValidationDFhistdf <- ValidationDFupdated[,c("recorder",'Start.time')]
 ValidationDFhistdf$DataSet <- rep('Automated',nrow(ValidationDFupdated))
@@ -127,21 +135,25 @@ ValidationDFhistdf$DataSet <- rep('Automated',nrow(ValidationDFupdated))
 gibbon.annotationshistdf <- gibbon.annotations[,c("Recorder",'Start.time')]
 gibbon.annotationshistdf$DataSet <- rep('Manual',nrow(gibbon.annotationshistdf))
 
+# Combine into a dataframe for plotting
 AnnotationsCombinedDF <- 
   rbind.data.frame(ValidationDFhistdf[,c("DataSet",'Start.time')],gibbon.annotationshistdf[,c("DataSet",'Start.time')]  )
 
+
+# Histogram of automated versus manual ------------------------------------
 # Note: need to change to today's date
 gghistogram(data=AnnotationsCombinedDF,x='Start.time',fill='DataSet',facet.by = 'DataSet',scales='free')+
   
-  scale_x_datetime(limits = ymd_h(c("2023-01-20 11", "2023-01-20 16"))) + theme(legend.position = "none")+
+  scale_x_datetime(limits = ymd_h(c("2023-01-23 11", "2023-01-23 16"))) + theme(legend.position = "none")+
   scale_fill_manual(values=c('blue','yellow') )+xlab('Time')+ylab('Count')
 
+
+# KS test on Unix values --------------------------------------------------
 AutomatedSub<- subset(AnnotationsCombinedDF,DataSet=='Automated')
 ManualSub<- subset(AnnotationsCombinedDF,DataSet=='Manual')
 
-plot(density(as.numeric(AutomatedSub$Start.time)))
-plot(density(as.numeric(ManualSub$Start.time)))
-
+# Convert times to Unix time by using as.numeric, then do ks test 
 ks.test(as.numeric(AutomatedSub$Start.time),
         as.numeric(ManualSub$Start.time))
+
 
