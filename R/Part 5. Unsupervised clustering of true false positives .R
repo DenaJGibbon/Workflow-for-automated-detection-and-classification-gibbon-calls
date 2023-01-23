0852b5da96bb76a40161a8cc27dcb24b62a68886
Dena Clink <denaclink@Denas-MacBook-Pro.local>
2023-01-23 09:38:46 -0500

    Update code.

--- R/Part 5. Unsupervised clustering of true false positives .R
+++ R/Part 5. Unsupervised clustering of true false positives .R
@@ -1,15 +1,13 @@
-library(plyr)
-library(stringr)
-library(ggpubr)
-library(apcluster)
-library(tuneR)
-library(aricode)
-library(clValid)
 library(gibbonR)
 library(dplyr)
+library(plyr)
 library(tidyr)
 library(ggpubr)
+library(umap)
+library(apcluster)
+library(cowplot)
 
+# Set seed for reproducibility
 set.seed(13)
 
 # True and False Positives ------------------------------------------------
@@ -17,34 +15,44 @@ set.seed(13)
 # NOTE: You must change this to the location where you have stored the downloaded data
 input.dir <- '/Volumes/DJC Files/Clink et al Zenodo Data/'
 
+# Read in function 
 source('R/MFCCFunctionMeanSD.R')
+
+# Set input directory
 subset.directory <- paste(input.dir, 'TrueFalsePositives', sep='')
 
+# Calculate MFCCs
 trainingdata <- MFCCFunctionMeanSD(input.dir=subset.directory , min.freq = 500, max.freq = 1600)
 
+# Isolate true/false indicator
 traningdatanames <- list.files(subset.directory,
                                full.names = F,pattern = '.wav')
 
+# Create new column indicating wheter the detection was a true/false positive
 trainingdata$Class <- str_split_fixed(traningdatanames,pattern = '_',n=2)[,1]
 
-
+# Convert the column to a factor
 trainingdata$Class <- as.factor(trainingdata$Class)
 
-
+# Run umap
 AcousticSignalsMFCC.umap <-
   umap::umap(trainingdata[,-c(1,51)],
              #labels=as.factor(trainingdata$Class),
              controlscale=TRUE,scale=3)
 
+# Create new dataframe for umap
 plot.for.AcousticSignalsMFCC <-
   cbind.data.frame(AcousticSignalsMFCC.umap$layout[,1:2],
                    trainingdata$Class)
 
+# Add informative column names
 colnames(plot.for.AcousticSignalsMFCC) <-
   c("Dim.1", "Dim.2", "Class")
 
+# Convert 'Class' to factor
 plot.for.AcousticSignalsMFCC$Class <- as.factor(plot.for.AcousticSignalsMFCC$Class)
 
+# Revalue 'overlap' calls to 'yes'
 plot.for.AcousticSignalsMFCC$Class <- revalue(plot.for.AcousticSignalsMFCC$Class,
                                               c(o = 'y',oo='y'))
 
@@ -65,11 +73,7 @@ Plot1 <- ggpubr::ggscatter(data = plot.for.AcousticSignalsMFCC,x = "Dim.1",
         axis.ticks.y=element_blank()  #remove y axis ticks
   )+scale_color_manual(values=c('orange','blue') )
 
-ggpubr::ggscatter(data = plot.for.AcousticSignalsMFCC,x = "Dim.1",
-                  y = "Dim.2",
-                  color  = "Class", shape='Class', alpha=0.1,facet.by = 'Class')
 Plot1
-
 
 
 # Supervised classification -------------------------------------------------
@@ -83,30 +87,35 @@ ml.model.svm <- e1071::svm(trainingdata[,-c(1,51)], trainingdata$Class, kernel =
 print(paste('SVM accuracy',ml.model.svm$tot.accuracy))
 
 # Unsupervised clustering -------------------------------------------------
-
+# Run affinity propagation clustering
+# NOTE: this takes a long time to run!
 AcousticSignalsAP <-
   apcluster::apcluster(negDistMat(r=2),q= 0.1,
                        trainingdata[,-c(1,51)],
                        maxits=100000,convits=10000)
 
-
+# Calculate NMI to see how well clusters match with true/false labels
 aricode::NMI(as.factor(AcousticSignalsAP@idx),trainingdata$class)
 
+# Run umap
 AcousticSignals.umap <-
   umap::umap(trainingdata[,-c(1,51)],
              #labels=as.factor( as.numeric(AcousticSignalsAP@idx)),
              controlscale=TRUE,scale=3)
 
+# Combine into dataframe for plotting
 plot.for.AcousticSignals <-
   cbind.data.frame(AcousticSignals.umap$layout[,1:2],#VGGishDF$PercentClass,
                    as.factor( as.numeric(AcousticSignalsAP@idx)))
 
+# Provide new column names
 colnames(plot.for.AcousticSignals) <-
   c("Dim.1", "Dim.2","Cluster")
 
+# Assign cluster as factor
 plot.for.AcousticSignals$Cluster <- as.factor(plot.for.AcousticSignals$Cluster)
-plot.for.AcousticSignals$Class <- trainingdata$Class
 
+# Plot the results
 Plot2 <- ggpubr::ggscatter(data = plot.for.AcousticSignals,x = "Dim.1",
                   y = "Dim.2",
                   color='Cluster', alpha=0.4) + guides(color='none')+ggtitle('Affinity propagation')+
@@ -116,8 +125,7 @@ Plot2 <- ggpubr::ggscatter(data = plot.for.AcousticSignals,x = "Dim.1",
         axis.ticks.y=element_blank()  #remove y axis ticks
   )+scale_color_manual(values= viridis::turbo ( length(AcousticSignalsAP@exemplars)  ) )
 
-
-
+# Combine the two plots using cowplot 
 cowplot::plot_grid(Plot1,Plot2)
 
 
